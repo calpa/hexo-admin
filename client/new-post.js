55a0bdd377487cc286f8002780175eac74e4cb6d
Calpa Liu <calpaliu@gmail.com>
2017-09-16 22:45:01 +0800

    Refactor newPost

--- client/new-post.js
+++ client/new-post.js
@@ -1,96 +1,96 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { newPost } from './api';
 
-const api = require('./api');
-
-const NewPost = React.createClass({
-  getInitialState() {
-    return {
+class NewPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       showing: false,
       loading: true,
-      text: 'Untitled',
+      text: 'Your Post Title', // @TODO i18n
     };
-  },
+  }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.showing && !prevState.showing) {
-      const node = this.refs.input.getDOMNode();
-      node.focus();
-      node.selectionStart = 0;
-      node.selectionEnd = node.value.length;
-    }
-  },
-
-  _onKeydown(e) {
+  onKeydown(e) {
     if (e.key === 'Enter') {
-      this._onSubmit(e);
+      this.onSubmit(e);
     }
-  },
+  }
 
-  _onShow() {
-    this.setState({ showing: true });
-  },
-
-  _onBlur() {
-    if (this.state.showing) {
-      this._onCancel();
-    }
-  },
-
-  _onSubmit(e) {
+  onSubmit(e) {
     e.preventDefault();
     this.setState({ loading: true, showing: false });
-    api.newPost(this.state.text).then((post) => {
+    newPost(this.state.text).then((post) => {
       this.setState({ showing: false, text: 'Untitled' });
       this.props.onNew(post);
     }, (err) => {
       console.error('Failed! to make post', err);
     });
-  },
+  }
 
-  _onCancel() {
-    this.setState({ showing: false });
-  },
 
-  _onChange(e) {
+  onChange(e) {
     this.setState({
       text: e.target.value,
     });
-  },
+  }
+
+  focusTextInput() {
+    this.textInput.focus();
+  }
+
+  handleBlur() {
+    if (this.state.showing) {
+      this.toggleInput(false);
+    }
+  }
+
+  toggleInput(showing) {
+    this.setState(() => ({ showing }));
+  }
 
   render() {
     if (!this.state.showing) {
-      return (<div className="new-post" onClick={this._onShow}>
-        <div className="new-post_button">
-          <i className="fa fa-plus" />{' '}
-          New Post
+      return (
+        <div
+          className="new-post"
+          onClick={() => this.toggleInput(true)}
+          role="button"
+          aria-hidden
+        >
+          <div className="new-post_button">
+            <i className="fa fa-plus" />{' '}
+            New Post
+          </div>
         </div>
-      </div>);
+      );
     }
 
-    return (<div className="new-post">
-      <input
-        className="new-post_input"
-        ref="input"
-        value={this.state.text}
-        onBlur={this._onBlur}
-        onKeyPress={this._onKeydown}
-        onChange={this._onChange}
-      />
-      <i
-        className="fa fa-check-circle new-post_ok"
-        onMouseDown={this._onSubmit}
-      />
-      <i
-        className="fa fa-times-circle new-post_cancel"
-        onMouseDown={this._onCancel}
-      />
-    </div>);
-  },
-});
-
-NewPost.propTypes = {
-  onNew: PropTypes.func,
-};
+    return (
+      <div
+        className="new-post"
+      >
+        <input
+          className="new-post_input"
+          value={this.state.text}
+          onBlur={() => this.handleBlur()}
+          onKeyPress={e => this.onKeydown(e)}
+          onChange={() => this.onChange()}
+        />
+        <i
+          className="fa fa-check-circle new-post_ok"
+          onMouseDown={e => this.onSubmit(e)}
+          role="button"
+          aria-hidden
+        />
+        <i
+          className="fa fa-times-circle new-post_cancel"
+          onMouseDown={() => this.toggleInput(false)}
+          role="button"
+          aria-hidden
+        />
+      </div>);
+  }
+}
 
 module.exports = NewPost;
